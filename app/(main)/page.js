c6ecd8a93adf0105fc09515c3a3543b01006c449
Sharif-Miah <farhansharif1410@gmail.com>
2025-11-04 21:16:49 +0600

    show data from mongodb

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -18,94 +18,13 @@ import Business from '@/assets/images/categories/business.jpg';
 import Photography from '@/assets/images/categories/photography.jpg';
 import Music from '@/assets/images/categories/music.jpg';
 import Course from '@/assets/images/courses/course_1.png';
+import { getCourseList } from '@/queries/courses';
+import { getCategories } from '@/queries/categories';
 
-const categories = [
-  {
-    id: 1,
-    title: 'Design',
-    thumbnail: Design,
-  },
-  {
-    id: 3,
-    title: 'Development',
-    thumbnail: Development,
-  },
-  {
-    id: 4,
-    title: 'Marketing',
-    thumbnail: Marketing,
-  },
-  {
-    id: 5,
-    title: 'IT & Software',
-    thumbnail: ItSoftware,
-  },
-  {
-    id: 6,
-    title: 'Personal Development',
-    thumbnail: Personal,
-  },
-  {
-    id: 7,
-    title: 'Business',
-    thumbnail: Business,
-  },
-  {
-    id: 8,
-    title: 'Photography',
-    thumbnail: Photography,
-  },
-  {
-    id: 9,
-    title: 'Music',
-    thumbnail: Music,
-  },
-];
+const HomePage = async () => {
+  const courses = await getCourseList();
+  const categories = await getCategories();
 
-const courses = [
-  {
-    id: 1,
-    title: 'Design',
-    thumbnail: '/assets/images/categories/design.jpg',
-  },
-
-  {
-    id: 3,
-    title: 'Development',
-    thumbnail: '/assets/images/categories/development.jpg',
-  },
-  {
-    id: 4,
-    title: 'Marketing',
-    thumbnail: '/assets/images/categories/marketing.jpg',
-  },
-  {
-    id: 5,
-    title: 'IT & Software',
-    thumbnail: '/assets/images/categories/it_software.jpg',
-  },
-  {
-    id: 6,
-    title: 'Personal Development',
-    thumbnail: '/assets/images/categories/personal_development.jpg',
-  },
-  {
-    id: 7,
-    title: 'Business',
-    thumbnail: '/assets/images/categories/business.jpg',
-  },
-  {
-    id: 8,
-    title: 'Photography',
-    thumbnail: '/assets/images/categories/photography.jpg',
-  },
-  {
-    id: 9,
-    title: 'Music',
-    thumbnail: '/assets/images/categories/music.jpg',
-  },
-];
-const HomePage = () => {
   return (
     <>
       <section className='space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32 grainy'>
@@ -155,7 +74,7 @@ const HomePage = () => {
           <SectionTitle>Categories</SectionTitle>
 
           <Link
-            href={''}
+            href={``}
             className=' text-sm font-medium  hover:opacity-80 flex items-center gap-1'>
             Browse All <ArrowRightIcon className='h-4 w-4' />
           </Link>
@@ -164,12 +83,12 @@ const HomePage = () => {
           {categories.map((category) => {
             return (
               <Link
-                href=''
+                href={`/categories/${category.id}`}
                 key={category.id}
                 className='relative overflow-hidden rounded-lg border bg-background p-2 hover:scale-105 transition-all duration-500 ease-in-out'>
                 <div className='flex  flex-col gap-4 items-center justify-between rounded-md p-6'>
                   <Image
-                    src={category.thumbnail}
+                    src={`/assets/images/categories/${category.thumbnail}`}
                     alt={category.title}
                     width={100}
                     height={100}
@@ -189,37 +108,39 @@ const HomePage = () => {
         <div className='flex items-center justify-between'>
           <SectionTitle>Courses</SectionTitle>
           <Link
-            href={''}
+            href={'/courses'}
             className=' text-sm font-medium  hover:opacity-80 flex items-center gap-1'>
             Browse All <ArrowRightIcon className='h-4 w-4' />
           </Link>
         </div>
         <div className='grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4'>
-          {courses.map((category) => {
+          {courses.map((course) => {
             return (
               <Link
-                key={category.id}
-                href={`/courses/${category.id}`}>
+                key={course.id}
+                href={`/courses/${course.id}`}>
                 <div className='group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full'>
                   <div className='relative w-full aspect-video rounded-md overflow-hidden'>
                     <Image
-                      src={Course}
-                      alt={'course'}
+                      src={course?.thumbnail}
+                      alt={course?.title}
                       className='object-cover'
                       fill
                     />
                   </div>
                   <div className='flex flex-col pt-2'>
                     <div className='text-lg md:text-base font-medium group-hover:text-sky-700 line-clamp-2'>
-                      Reactive Accelerator
+                      {course?.title}
                     </div>
-                    <p className='text-xs text-muted-foreground'>Development</p>
+                    <p className='text-xs text-muted-foreground'>
+                      {course?.category?.title}
+                    </p>
                     <div className='my-3 flex items-center gap-x-2 text-sm md:text-xs'>
                       <div className='flex items-center gap-x-1 text-slate-500'>
                         <div>
                           <BookOpen className='w-4' />
                         </div>
-                        <span>4 Chapters</span>
+                        <span>{course?.modules?.length} Chapters</span>
                       </div>
                     </div>
 
@@ -231,7 +152,7 @@ const HomePage = () => {
 
                     <div className='flex items-center justify-between mt-4'>
                       <p className='text-md md:text-sm font-medium text-slate-700'>
-                        {formatPrice(49)}
+                        {formatPrice(course?.price)}
                       </p>
 
                       <Button
