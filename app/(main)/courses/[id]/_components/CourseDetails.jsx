2919f938bbff54bdd2f681ec949df5d425176ef3
Sharif-Miah <farhansharif1410@gmail.com>
2025-11-10 20:06:46 +0600

    course details component data saw from server

--- app/(main)/courses/[id]/_components/CourseDetails.jsx
+++ app/(main)/courses/[id]/_components/CourseDetails.jsx
@@ -25,33 +25,40 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/accordion';
+import { formatMyDate } from '@/lib/date';
 
-const CourseDetails = () => {
+const CourseDetails = ({ course }) => {
+  console.log(course);
+
+  const lastModifiedDate = formatMyDate(course.modifiedOn);
+
   return (
     <section className='py-8 md:py-12 lg:py-24'>
       <div className='container'>
-        <span className='bg-success px-4 py-0.5 rounded-full text-xs font-medium text-white inline-block'>
-          Development
+        <span className='bg-success px-4 py-0.5 rounded-full text-xs font-medium bg-green-400 text-white inline-block'>
+          {course?.category?.title}
         </span>
         <h3 className='text-2xl md:text-3xl lg:text-4xl font-bold 2xl:text-5xl mt-3'>
-          Reactive Accelerator
+          {course?.title}
         </h3>
-        <p className='mt-3 text-gray-600 text-sm'>Master React JS & Next JS</p>
+        <p className='mt-3 text-gray-600 text-sm'>{course?.subtitle}</p>
         {/*  */}
         <div className='flex sm:items-center gap-5 flex-col sm:flex-row sm:gap-6 md:gap-20 mt-6'>
           <div className='flex items-center gap-2'>
             <Image
               className='w-[40px] h-[40px] rounded-full'
-              src='https://avatars.githubusercontent.com/u/3633137?v=4'
-              alt='sumit saha'
+              src={course?.instructor?.profilePicture}
+              alt={course?.instructor?.firstName}
               width={40}
               height={40}
             />
-            <p className='font-bold'>Tapas Adhikary</p>
+            <p className='font-bold'>
+              {course?.instructor?.firstName} {course?.instructor?.lastName}
+            </p>
           </div>
           <div className='flex items-center gap-2 text-sm'>
             <span className='text-success font-semibold'>Last Updated: </span>
-            <span>Feb 22, 2022</span>
+            <span>{lastModifiedDate}</span>
           </div>
         </div>
 
