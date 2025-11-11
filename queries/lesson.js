a3fee352296960db39fea49b938daf1b471aea6e
Sharif-Miah <farhansharif1410@gmail.com>
2025-11-11 22:05:36 +0600

    course Details page work done

--- queries/lesson.js
+++ queries/lesson.js
@@ -1,5 +1,5 @@
-import { Lesson } from '@/model/lesson.model';
 import { replaceMongoIdInObject } from '@/lib/convertData';
+import { Lesson } from '@/model/lesson-model';
 
 export async function getLesson(lessonId) {
   const lesson = await Lesson.findById(lessonId).lean();
