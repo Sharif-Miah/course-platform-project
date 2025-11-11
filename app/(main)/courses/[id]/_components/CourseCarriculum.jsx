984807949dc625b7b126d7a158a2644d87388f75
Sharif-Miah <farhansharif1410@gmail.com>
2025-11-11 21:09:06 +0600

    total hours show data in backend

--- app/(main)/courses/[id]/_components/CourseCarriculum.jsx
+++ app/(main)/courses/[id]/_components/CourseCarriculum.jsx
@@ -1,4 +1,3 @@
-import { Star } from 'lucide-react';
 import { BookCheck } from 'lucide-react';
 import { Clock10 } from 'lucide-react';
 import { Radio } from 'lucide-react';
@@ -18,17 +17,23 @@ import {
 } from '@/components/ui/accordion';
 import { cn } from '@/lib/utils';
 
-const CourseCarriculum = () => {
+const CourseCarriculum = ({ course }) => {
+  const totalDuration = course?.modules.reduce(function (acc, obj) {
+    return acc + obj.duration;
+  }, 0);
+
+  console.log('all course', totalDuration);
+
   return (
     <div>
       <div class='flex gap-x-5 items-center justify-center flex-wrap mt-4 mb-6 text-gray-600 text-sm'>
         <span className='flex items-center gap-1.5'>
           <BookCheck className='w-4 h-4' />
-          12 Chapters
+          {course?.modules?.length} Chapters
         </span>
         <span className='flex items-center gap-1.5'>
           <Clock10 className='w-4 h-4' />
-          50+ Hours
+          {(totalDuration / 60).toPrecision(2)} Hours
         </span>
         <span className='flex items-center gap-1.5'>
           <Radio className='w-4 h-4' />4 Live Class
