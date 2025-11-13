01dfbf593e0dc5fca0228b0c3f26a45295b3a112
Sharif-Miah <farhansharif1410@gmail.com>
2025-11-13 21:12:48 +0600

    login from function make but not write logic

--- app/login/_components/login-form.jsx
+++ app/login/_components/login-form.jsx
@@ -12,6 +12,10 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
 export function LoginForm() {
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <Card className='mx-auto max-w-sm w-full'>
       <CardHeader>
@@ -21,35 +25,39 @@ export function LoginForm() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className='grid gap-4'>
-          <div className='grid gap-2'>
-            <Label htmlFor='email'>Email</Label>
-            <Input
-              id='email'
-              type='email'
-              placeholder='m@example.com'
-              required
-            />
-          </div>
-          <div className='grid gap-2'>
-            <div className='flex items-center'>
-              <Label htmlFor='password'>Password</Label>
-              {/* <Link href="#" className="ml-auto inline-block text-sm underline">
+        <form onSubmit={onSubmit}>
+          <div className='grid gap-4'>
+            <div className='grid gap-2'>
+              <Label htmlFor='email'>Email</Label>
+              <Input
+                id='email'
+                name='email'
+                type='email'
+                placeholder='m@example.com'
+                required
+              />
+            </div>
+            <div className='grid gap-2'>
+              <div className='flex items-center'>
+                <Label htmlFor='password'>Password</Label>
+                {/* <Link href="#" className="ml-auto inline-block text-sm underline">
                 Forgot your password?
               </Link> */}
+              </div>
+              <Input
+                id='password'
+                name='password'
+                type='password'
+                required
+              />
             </div>
-            <Input
-              id='password'
-              type='password'
-              required
-            />
+            <Button
+              type='submit'
+              className='w-full'>
+              Login
+            </Button>
           </div>
-          <Button
-            type='submit'
-            className='w-full'>
-            Login
-          </Button>
-        </div>
+        </form>
         <div className='mt-4 text-center text-sm'>
           Don&apos;t have an account?{' '}
           <Link
