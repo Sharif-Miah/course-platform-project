c1b0522a7f787833cdbeb308f39f55989f6b1da4
Sharif-Miah <farhansharif1410@gmail.com>
2025-11-13 20:20:55 +0600

    login component added

--- app/login/page.js
+++ app/login/page.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import { LoginForm } from './_components/login-form';
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className='w-full flex-col h-screen flex items-center justify-center'>
+      <div className='container'>
+        <LoginForm />
+      </div>
+    </div>
+  );
 };
-
 export default LoginPage;
