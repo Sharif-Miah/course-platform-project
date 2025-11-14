c43ccb04b11dfe5f2672426812a50b43bdd01e34
Sharif-Miah <farhansharif1410@gmail.com>
2025-11-14 20:23:13 +0600

    successfull login by crediantial

--- app/login/_components/login-form.jsx
+++ app/login/_components/login-form.jsx
@@ -14,6 +14,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { ceredntialLogin } from '@/app/actions';
 
 export function LoginForm(event) {
   const [error, setError] = useState('');
